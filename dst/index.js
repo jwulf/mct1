2bfe3eaa0761dc0e45c0815fbccbef60e99aa300
Joshua Wulf <josh@magikcraft.io>
2017-07-21 00:16:43 +1000

    Built

--- dst/index.js
+++ dst/index.js
@@ -1,11 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var env_1 = require("./util/env");
+var log_1 = require("./util/log");
 var BGLBarGlucoseMonitor_1 = require("./GlucoseMonitor/BGLBarGlucoseMonitor/BGLBarGlucoseMonitor");
 var mct1_1 = require("./util/mct1");
 var T1Player_1 = require("./Player/T1Player");
 mct1_1.mct1.version = '1.3.0';
-env_1.log("MCT1 version " + mct1_1.mct1.version);
+log_1.log("MCT1 version " + mct1_1.mct1.version);
 function controller(cmd) {
     if (cmd === void 0) { cmd = 'default'; }
     if (!mct1_1.mct1.initialised) {
@@ -15,7 +15,7 @@ function controller(cmd) {
         return processCmd(cmd);
     }
     function processCmd(cmd) {
-        env_1.log("Yo, mct1 executing " + cmd);
+        log_1.log("Yo, mct1 executing " + cmd);
         var controlr = mct1_1.mct1.controller;
         if (cmd === 'default') {
             (mct1_1.mct1.running) ? controlr.stop() : controlr.start();
@@ -32,7 +32,7 @@ function controller(cmd) {
         }
     }
     function initialise(callback) {
-        env_1.log('Initialising...');
+        log_1.log('Initialising...');
         var player = new T1Player_1.T1Player();
         mct1_1.mct1.BGLBar = new BGLBarGlucoseMonitor_1.BGLBarGlucoseMonitor(player, 1000);
         mct1_1.mct1.T1Player = player;
