14389277e3439816d8dc3c2c6b4368760bed9af8
The Legendary Josh Wulf™ - As Seen on the Internet® <josh@magikcraft.io>
2017-07-22 22:52:12 +1000

    Add comments

--- src/GlucoseMonitor/BGLBarGlucoseMonitor/BGLBarGlucoseMonitor.ts
+++ src/GlucoseMonitor/BGLBarGlucoseMonitor/BGLBarGlucoseMonitor.ts
@@ -7,6 +7,7 @@ export class BGLBarGlucoseMonitor extends GlucoseMonitor {
 
     constructor(player: T1Player, sampleRate: number) {
         super(player, sampleRate);
+        // Pass the player's BGL in to the bar, so the bar has access to its range alerts.
         this.BGLBar = new BGLBar(this.player.BGL, {
             Bars: magikcraft.io.Bars,
             sender: magikcraft.io.getSender(),
@@ -15,6 +16,7 @@ export class BGLBarGlucoseMonitor extends GlucoseMonitor {
     }
 
     monitor() {
+        // We don't need to pass in the BGL value, because the bar has a reference to the player's BGL
         this.BGLBar.update();
     }
 }
