189677a44656068946993188181fc48cbf13cdf2
The Legendary Josh Wulf™ - As Seen on the Internet® <josh@magikcraft.io>
2017-07-22 16:40:06 +1000

    export _default

--- dst/index.js
+++ dst/index.js
@@ -6,39 +6,20 @@ var mct1_1 = require("./util/mct1");
 var T1Player_1 = require("./Player/T1Player");
 mct1_1.mct1.version = '1.3.0';
 log_1.log("MCT1 version " + mct1_1.mct1.version);
-function controller(cmd) {
-    if (cmd === void 0) { cmd = 'default'; }
+function _default() {
     if (!mct1_1.mct1.initialised) {
-        return initialise(function () { return processCmd(cmd); });
-    }
-    else {
-        return processCmd(cmd);
-    }
-    function processCmd(cmd) {
-        log_1.log("Yo, mct1 executing " + cmd);
-        var controlr = mct1_1.mct1.controller;
-        if (cmd === 'default') {
-            (mct1_1.mct1.running) ? controlr.stop() : controlr.start();
-            return;
-        }
-        if (cmd === 'start') {
-            return controlr.start();
-        }
-        if (cmd === 'stop') {
-            return controlr.stop();
-        }
-        if (cmd === 'reset') {
-            return controlr.reset();
-        }
-    }
-    function initialise(callback) {
-        log_1.log('Initialising...');
-        var player = new T1Player_1.T1Player();
-        mct1_1.mct1.BGLBar = new BGLBarGlucoseMonitor_1.BGLBarGlucoseMonitor(player, 1000);
-        mct1_1.mct1.T1Player = player;
-        mct1_1.mct1.initialised = true;
-        mct1_1.mct1.running = false;
-        callback && callback();
+        initialise();
     }
 }
-exports.controller = controller;
+exports.spells = {
+    "default": _default
+};
+function initialise(callback) {
+    log_1.log('Initialising...');
+    var player = new T1Player_1.T1Player();
+    mct1_1.mct1.BGLBar = new BGLBarGlucoseMonitor_1.BGLBarGlucoseMonitor(player, 1000);
+    mct1_1.mct1.T1Player = player;
+    mct1_1.mct1.initialised = true;
+    mct1_1.mct1.running = false;
+    callback && callback();
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import { log } from './util/log';
-import { Interval } from './util/timer';
 import { BGLBarGlucoseMonitor } from './GlucoseMonitor/BGLBarGlucoseMonitor/BGLBarGlucoseMonitor';
 import { mct1 } from './util/mct1';
 import { T1Player } from './Player/T1Player';
@@ -7,41 +6,22 @@ import { T1Player } from './Player/T1Player';
 mct1.version = '1.3.0';
 log(`MCT1 version ${mct1.version}`);
 
-export function controller(cmd = 'default') {
+function _default() {
     if (!mct1.initialised) {
-        return initialise(() => processCmd(cmd));
-    } else {
-        return processCmd(cmd);
+        initialise();
     }
+}
 
-    function processCmd(cmd: string) {
-        log(`Yo, mct1 executing ${cmd}`);
-        const controlr = mct1.controller;
-        if (cmd === 'default') {
-            (mct1.running) ? controlr.stop() : controlr.start();
-            return;
-        }
-        if (cmd === 'start') {
-            return controlr.start();
-        }
-        if (cmd === 'stop') {
-            return controlr.stop();
-        }
-        if (cmd === 'reset') {
-            return controlr.reset();
-        }
-    }
+export const spells = {
+    "default": _default
+}
 
-    function initialise(callback: () => void) {
-
-        log('Initialising...');
-
-        const player = new T1Player();
-        mct1.BGLBar = new BGLBarGlucoseMonitor(player, 1000);
-        mct1.T1Player = player;
-
-        mct1.initialised = true;
-        mct1.running = false;
-        callback && callback();
-    }
+function initialise(callback?: () => void) {
+    log('Initialising...');
+    const player = new T1Player();
+    mct1.BGLBar = new BGLBarGlucoseMonitor(player, 1000);
+    mct1.T1Player = player;
+    mct1.initialised = true;
+    mct1.running = false;
+    callback && callback();
 }
